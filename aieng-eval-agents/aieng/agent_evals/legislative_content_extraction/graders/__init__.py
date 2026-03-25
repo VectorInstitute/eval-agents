f34e3a80afa4e15234554238f76d7e3df1e8ade0
SwapnaKaladharbabuTR <swapna.kaladhar@gmail.com>
2026-03-25 17:23:01 +0000

    added trace evaluator

--- aieng-eval-agents/aieng/agent_evals/legislative_content_extraction/graders/__init__.py
+++ aieng-eval-agents/aieng/agent_evals/legislative_content_extraction/graders/__init__.py
@@ -1,5 +1,6 @@
 """Graders for legislative content extraction evaluation."""
 
 from .item import item_level_deterministic_grader
+from .trace import trace_deterministic_grader
 
-__all__ = ["item_level_deterministic_grader"]
+__all__ = ["item_level_deterministic_grader", "trace_deterministic_grader"]
